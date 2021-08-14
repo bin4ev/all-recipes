b56a1840f77feebcb79e0183ce01d5cbcdfed4a0
Georgi Maksimov <bin4ev@gmail.com>
2021-08-15 01:00:08 +0300

    host app

--- my-app/all-recipes/src/environments/environment.prod.ts
+++ my-app/all-recipes/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyAO2hwQ2wiG8qz-mcXQxnHj-mfghOJBjK8",
+    authDomain: "angular-525d1.firebaseapp.com",
+    projectId: "angular-525d1",
+    storageBucket: "angular-525d1.appspot.com",
+    messagingSenderId: "211390953252",
+    appId: "1:211390953252:web:6b0cb3349886da8020e5b1"
+  },
 };
